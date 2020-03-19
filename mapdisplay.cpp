854707cb49af4b41a1143f7fae188d1d718bba4b
rocas777 <lmiguelapinto@gmail.com>
2020-03-19 13:14:28 +0000

    class mapdisplay

---
+++ mapdisplay.cpp
@@ -0,0 +1,6 @@
+#include "mapdisplay.h"
+
+mapDisplay::mapDisplay()
+{
+
+}
---
+++ mapdisplay.h
@@ -0,0 +1,11 @@
+#ifndef MAPDISPLAY_H
+#define MAPDISPLAY_H
+
+
+class mapDisplay
+{
+public:
+    mapDisplay();
+};
+
+#endif // MAPDISPLAY_H
